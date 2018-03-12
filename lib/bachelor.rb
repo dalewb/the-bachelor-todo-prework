59980546bdf4a0593fa18d6473b425bd4d1aba28
William Dale <williambdale@gmail.com>
2018-03-12 22:45:32 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,31 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |season_num, people|
+    if season_num == season 
+      # binding.pry 
+      people.each do |person|
+        # binding.pry 
+        if person[:status] == "Winner"
+          return person[:name].split(' ')[0]
+        end 
+      end 
+    end 
+  end 
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  
 end
 
 def get_occupation(data, hometown)
-  # code here
+  
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  
 end
