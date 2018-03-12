deb36ca2f8b52fd095cad4855664eef0f27271a5
William Dale <williambdale@gmail.com>
2018-03-12 23:39:37 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -46,6 +46,7 @@ end
 
 def get_average_age_for_season(data, season)
   age = 0 
+  count = 0 
   data.each do |season_num, people|
     if season_num == season  
       people.each do |person|
@@ -54,5 +55,5 @@ def get_average_age_for_season(data, season)
       end 
     end 
   end 
-  avg
+  avg = (age/count)
 end
